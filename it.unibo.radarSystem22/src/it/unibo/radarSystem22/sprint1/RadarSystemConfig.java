4a054679283e98cb39cf5d8f946e13c8c42bed90
robertocalvani <robertocalvanillt@gmail.com>
2022-03-21 16:58:33 +0100

    sprint 2 e 2a

--- it.unibo.radarSystem22/src/it/unibo/radarSystem22/sprint1/RadarSystemConfig.java
+++ it.unibo.radarSystem22/src/it/unibo/radarSystem22/sprint1/RadarSystemConfig.java
@@ -6,7 +6,6 @@ import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
@@ -23,24 +22,27 @@ public class RadarSystemConfig {
  	public static String hostAddr         = "localhost";		
 	public static String raspAddr         = "localhost";		
 	public static int serverPort          = 8023;
+//Aggiunte dello SPRINT2a 	
+	public static int ledPort             = 8010;
+	public static int sonarPort           = 8015;
  	
 	
 	
-	public static void setTheConfiguration(  ) throws JSONException {
+	public static void setTheConfiguration(  ) {
 		setTheConfiguration("../RadarSystemConfig.json");
 	}
 	
-	public static void setTheConfiguration( String resourceName ) throws JSONException {
+	public static void setTheConfiguration( String resourceName ) {
 		//Nella distribuzione resourceName è in una dir che include la bin  
 		FileInputStream fis = null;
-		Reader r = null;
 		try {
 			ColorsOut.out("%%% setTheConfiguration from file:" + resourceName);
 			if(  fis == null ) {
  				 fis = new FileInputStream(new File(resourceName));
- 				 r = new InputStreamReader(fis);
 			}
-	        JSONTokener tokener = new JSONTokener(r);
+//	        JSONTokener tokener = new JSONTokener(fis);
+			Reader reader       = new InputStreamReader(fis);
+			JSONTokener tokener = new JSONTokener(reader);      
 	        JSONObject object   = new JSONObject(tokener);
 	 		
    	        tracing          = object.getBoolean("tracing");
@@ -51,6 +53,9 @@ public class RadarSystemConfig {
 	        serverPort		= object.getInt("serverPort");
  	        hostAddr 		= object.getString("hostAddr");
 	        raspAddr 		= object.getString("raspAddr");
+//Aggiunte dello SPRINT2a
+	        ledPort         = object.getInt("ledPort");
+	        sonarPort       = object.getInt("sonarPort");
 	        
 		} catch (FileNotFoundException e) {
  			ColorsOut.outerr("setTheConfiguration ERROR " + e.getMessage() );
